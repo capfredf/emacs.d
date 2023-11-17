c160d965b0c1b7638d16119cf7d117e0713fadcd
Fred Fu <moonsolo@gmail.com>
2023-11-17 15:10:14 -0500

    up

--- setting.el
+++ setting.el
@@ -210,7 +210,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:family "Sarasa Fixed SC" :slant normal :weight normal :height 120 :width normal))))
+ '(default ((t (:inherit nil :extend nil :stipple nil :background "#fdf6e3" :foreground "#657b83" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight regular :height 120 :width normal :foundry "ADBO" :family "Iosevka"))))
  '(fixed-pitch ((t (:family "Iosevka" :slant normal :weight normal :height 1.0 :width normal))))
  '(font-lock-variable-name-face ((t nil)))
  '(helm-ff-file-extension ((t (:extend t :foreground "dark red"))))
