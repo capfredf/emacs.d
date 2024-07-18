feed4a2ab5bac1e2fa932e9344da68c3cefdff87
Fred Fu <moonsolo@gmail.com>
2024-07-18 13:24:21 -0400

    update

--- init.el
+++ init.el
@@ -46,6 +46,7 @@
 (when (file-exists-p custom-file)
   (load custom-file))
 
+(set-fontset-font "fontset-default" 'symbol "Noto Color Symbols 2" nil 'append)
 (setq make-backup-files nil)
 (define-coding-system-alias 'UTF-8 'utf-8)
 (define-coding-system-alias 'utf8 'utf-8)
