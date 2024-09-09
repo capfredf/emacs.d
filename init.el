16512cbffbc922eb4b64e9193220ce6697e6155e
Fred Fu <moonsolo@gmail.com>
2024-09-09 10:57:25 -0400

    update

--- init.el
+++ init.el
@@ -122,14 +122,14 @@
 ;;   (helm-mode 1))
 
 
-(use-package which-key
-  :ensure t
-  :config
-  (add-to-list 'which-key-show-transient-maps text-scaling-map)
-  (setq which-key-show-early-on-C-h t)
-  (setq which-key-idle-delay 10000)
-  (setq which-key-idle-secondary-delay 0.05)
-  (which-key-mode))
+;; (use-package which-key
+;;   :ensure t
+;;   :config
+;;   (add-to-list 'which-key-show-transient-maps text-scaling-map)
+;;   (setq which-key-show-early-on-C-h t)
+;;   (setq which-key-idle-delay 10000)
+;;   (setq which-key-idle-secondary-delay 0.05)
+;;   (which-key-mode))
 
 (global-set-key (kbd "<f3>") 'scale-text)
 
