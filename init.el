ca6daa70602e811a4a758e1bd0f84decd44b7825
Fred Fu <moonsolo@gmail.com>
2025-03-12 13:55:32 -0400

    add my/increment-number-at-point

--- init.el
+++ init.el
@@ -821,3 +821,12 @@
 
 (use-package proof-general
   :ensure t)
+
+(defun my/increment-number-at-point ()
+  (interactive)
+  (skip-chars-backward "0-9")
+  (or (looking-at "[0-9]+")
+      (error "No number at point"))
+  (replace-match (number-to-string (1+ (string-to-number (match-string 0))))))
+
+(global-set-key (kbd "C-c +") #'my/increment-number-at-point)
