1b6c1af02d14885aa5a0bf00943edd3dd3f13888
Fred Fu <moonsolo@gmail.com>
2025-08-12 11:06:24 -0400

    change tabspaces settings

--- init.el
+++ init.el
@@ -982,6 +982,7 @@
   :commands (tabspaces-switch-or-create-workspace
              tabspaces-open-or-create-project-and-workspace)
   :custom
+  (tabspaces-session-project-session-store 'project)
   (tabspaces-use-filtered-buffers-as-default t)
   (tabspaces-default-tab "Default")
   (tabspaces-remove-to-default t)
@@ -989,7 +990,7 @@
   ;; (tabspaces-initialize-project-with-todo t)
   ;; (tabspaces-todo-file-name "project-todo.org")
   ;; sessions
-  ;; (tabspaces-session t)
+  (tabspaces-session t)
   ;; (tabspaces-session-auto-restore t)
   (tab-bar-new-tab-choice "*scratch*"))
 
